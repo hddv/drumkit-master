10aba1a22699e4500c15ceebb4f1a3fbb9f7071e
DINH VU Hien <dinhvuhien@MacBook-Pro-de-DINH.local>
2020-04-30 00:21:12 +0200

    bound sound and clicked button

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 // alert("hello")
-let drumButton = document.querySelectorAll("button.drum");
+let drumButton = document.querySelectorAll(".drum");
 
 drumButton.forEach(button => {
-  button.addEventListener("click", function(){alert("button clicked")})
+  button.addEventListener("click", function(){
+    //change the text color of the clicked button
+    // this.classList.add("white");
+
+    let arraySounds = {
+      s: "sounds/tom-1.mp3",
+      d: "sounds/tom-2.mp3",
+      f: "sounds/tom-3.mp3",
+      g: "sounds/tom-4.mp3",
+      j: "sounds/snare.mp3",
+      k: "sounds/crash.mp3",
+      l: "sounds/kick-bass.mp3"
+    }
+
+    //get the letter matchs with the sound
+    let letterButton = this.innerHTML;
+  
+    //play the matched sound 
+    (new Audio(arraySounds[letterButton])).play();
+  
+    
+  })
 });
